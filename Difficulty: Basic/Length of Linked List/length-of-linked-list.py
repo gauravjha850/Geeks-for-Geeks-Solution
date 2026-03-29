9d282371fbb045ff87eb84aaecc7228c0df99536
gauravjha850 <gauravjha850@gmail.com>
2026-03-30 04:08:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Length of Linked List/length-of-linked-list.py
@@ -0,0 +1,15 @@
+'''
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+'''
+class Solution:
+    def getCount(self, head):
+        count=0
+        while head is not None:
+            
+            count=count+1
+            head=head.next
+            
+        return count
