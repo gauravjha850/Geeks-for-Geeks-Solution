fcf1a550fa728cd2ce2348d844e68b12c43909a0
gauravjha850 <gauravjha850@gmail.com>
2026-04-12 20:28:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Asteroid Collision/asteroid-collision.py
@@ -0,0 +1,19 @@
+class Solution:
+    def asterCol(self, arr):
+        n=len(arr)
+        stack=[]
+        
+        for i in range (n):
+            if arr[i]>0:
+                stack.append(arr[i])
+            else:
+                while stack and stack[-1]>0 and stack[-1]<abs(arr[i]):
+                    stack.pop()
+                
+                if stack and stack[-1]==abs(arr[i]):
+                    stack.pop()
+                
+                elif not stack or stack[-1] <0:
+                    stack.append(arr[i])
+        return stack
+        
