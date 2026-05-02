dda25f5d7695e8554e69f49597f3b89cd4cb3897
gauravjha850 <gauravjha850@gmail.com>
2026-05-02 19:09:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.py
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.py
@@ -1,22 +1,21 @@
 class Solution:
     def celebrity(self, mat):
-        low=0
         n=len(mat)
-        high=n-1
-        while low  < high :
-            if mat[low][high]==1:
-                low=low+1
-            
-                
+        
+        a=0
+        b=n-1
+        
+        while a< b:
+            if mat[a][b]==1:
+                a=a+1
             else:
-                high=high-1
-                
-        
-        candidate = low
-        for i in range (n):
-            if i !=candidate :
-                
-        
+                b=b-1
+        candidate=a
+        for i in range(n):
+            if i!=candidate :
                 if mat[candidate][i]==1 or mat[i][candidate]==0:
+                    
                     return -1
-        return candidate
+        return  candidate 
+        
+        
