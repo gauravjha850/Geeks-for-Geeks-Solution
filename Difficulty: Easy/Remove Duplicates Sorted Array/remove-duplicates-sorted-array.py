9a321601c6d7d99d2097c71a5e8f8a4f5463c841
gauravjha850 <gauravjha850@gmail.com>
2026-06-01 11:48:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeDuplicates(self, arr):
+        if not arr:
+            return []
+        i=0
+        for j in range (1,len(arr)):
+            if arr[j]!=arr[i]:
+                i+=1
+                arr[i]=arr[j]
+        return arr[:i+1]
+        
